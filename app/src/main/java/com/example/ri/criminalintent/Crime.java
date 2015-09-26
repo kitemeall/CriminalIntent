5420a7d36f0da14e12bf2947c41e6718a67b0a1f
Rostislav Ivanov <kitemeall@gmail.com>
2015-09-26 22:11:10 +0300

    lalala

--- app/src/main/java/com/example/ri/criminalintent/Crime.java
+++ app/src/main/java/com/example/ri/criminalintent/Crime.java
@@ -1,13 +1,18 @@
 package com.example.ri.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
     public Crime(){
         //generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
--- app/src/main/java/com/example/ri/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/ri/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.example.ri.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -10,6 +12,16 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
     }
 
 
